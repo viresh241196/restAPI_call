be91e2d98b53af44e5ea0f487b83ceb8340d1f4c
viresh241196 <vireshrawool007@gmail.com>
2021-04-22 21:19:41 +0530

    added method to use query parameter to fetch the name

--- main/java/com/example/demo/controller/DemoRestController.java
+++ main/java/com/example/demo/controller/DemoRestController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,13 +15,14 @@ public class DemoRestController {
 	@RequestMapping( { "" , "/" , "/home" } )
 	public String sayHello() {
 	return "Hello from Bridgelabz!"; 
-		}
-	
-	@GetMapping("/hello1")
-	public String hello1(@RequestParam(name="name", required = false, defaultValue = "default") String name)
-	{
-	return "Hello " + name;
 	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(name="name") String name)
+	{
+		return "Hello " + name;
+	}
+	
 
 }
 	
