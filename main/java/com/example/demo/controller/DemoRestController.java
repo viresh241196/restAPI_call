9e0715dd56234a6a896fbcd9d90a09ceffce92a3
viresh241196 <vireshrawool007@gmail.com>
2021-04-23 06:44:05 +0530

    created a post method and added class to it

--- main/java/com/example/demo/controller/DemoRestController.java
+++ main/java/com/example/demo/controller/DemoRestController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.sun.java.util.jar.pack.Package.Class.Member;
 
 @RestController
 @RequestMapping("/demo")
@@ -30,6 +37,27 @@ public class DemoRestController {
 	  {
 	      return "Hello " + name;
 	  }
+//	  @PostMapping("/hello2/{name}")
+//	  public String hello2(@PathVariable("name") String name)
+//	  {
+//	      return "Hello " + name;
+//	  }
+	  
+//	  @RequestMapping(value = "/persistPerson", method = RequestMethod.POST)
+//	  public ResponseEntity<String> persistPerson(@RequestBody DTO) {
+//
+//	      return ResponseEntity.status(HttpStatus.CREATED).build();
+//	    }
+	  
+//	  @PostMapping(value = "/persistPerson", method = RequestMethod.POST)
+//	  public ResponseEntity<String> persistPerson(@RequestBody PersonDTO person) {
+//	      return ResponseEntity.status(HttpStatus.CREATED).build();
+//	  }
+	  
+	  @PostMapping("/post")
+	    public String sayHello(@RequestBody User user) {
+	        return "Hello" + user.getFirstName() + " " +user.getLastName()+"!";
+	    }
 }
 	
 
---
+++ main/java/com/example/demo/model/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return firstName;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return lastName;
+	}
+
+}
